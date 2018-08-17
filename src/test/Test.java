4310780c1230bf9b5fbafd34667f97b5d3f5cff1
George <djordjerpetrovic@yahoo.com>
2018-08-17 16:58:49 +0200

    Test

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello, world, again!");
+		System.out.println("Hello, world, again! Yes");
 	}
 }
