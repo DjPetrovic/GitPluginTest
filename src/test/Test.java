945a9a59e0b3f9f29cac8ffd3a07c268260f9947
George <djordjerpetrovic@yahoo.com>
2018-08-20 13:34:22 +0200

    #1234

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello, world, again! Yes");
+		System.out.println("Hello, git, from branch bug#1234 !!");
 	}
 }
